6f1a1df1af4801286b50c930c27a6dde1d8ae567
Fadhl Al Eryani <f@MacBook-Pro-3.local>
2023-01-14 00:40:27 +0100

    added alif rules

--- src/malti2arabi.py
+++ src/malti2arabi.py
@@ -1,10 +1,5 @@
 import pynini as pn
 
-def translit(string):
-    rules = (malti2safebw.closure() @ shadda_rule)
-    return ( string @ rules.optimize()).rmepsilon()
-
-
 # basic map
 malti2safebw = pn.string_file('../rules/malti2safebw.cross')
 
@@ -16,3 +11,16 @@ safebw_sigma = pn.string_file('../chars/safebw.sym')
 shaddas = pn.string_file('../rules/shadda.cross')
 shadda_rule_sigma = pn.union(safebw_sigma,'~')
 shadda_rule = pn.cdrewrite(shaddas,'','', shadda_rule_sigma.closure()).optimize()
+
+# alif_init rule
+alif_inits = pn.string_file('../rules/alif_init.cross')
+alif_init_rule = pn.cdrewrite(alif_inits,'[BOS]','', safebw_sigma.closure()).optimize()
+
+# alif_mid rule
+alif_mid = pn.cross('ie','A')
+alif_mid_rule = pn.cdrewrite(alif_mid,'','', safebw_sigma.closure()).optimize()
+
+
+def translit(string):
+    rules = malti2safebw.closure() @ shadda_rule @ alif_init_rule @ alif_mid_rule
+    return  string @ rules.optimize()
