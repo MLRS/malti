752c6aa43324408475fc3a0f642825dd73c9a117
fadhleryani <eryani.f.a@gmail.com>
2023-05-16 18:20:48 +0200

    last push before merging with kurts code

--- src/malti2arabi_fst.py
+++ src/malti2arabi_fst.py
@@ -1,50 +1,75 @@
 import pynini as pn
+# 
 
+malti2arabi_2char_nondet = pn.string_file('mappings/malti2arabi_2char.map').optimize()
+malti2arabi_nondet = pn.string_file('mappings/malti2arabi_1char.map').optimize()
+shadda_nondet = pn.string_file('mappings/shadda.map').optimize()
+final_vowels_nondet = pn.string_file('mappings/final_vowels.map').optimize()
+special_nondet = pn.string_file('mappings/special.map').optimize()
+alif_initial_nondet = pn.string_file('mappings/alif_initial.map').optimize()
 
-malti2arabi_2char = pn.string_file('mappings/malti2arabi_2char.map').optimize()
 arabic2arabic = pn.string_file('mappings/arabic2arabic.map').optimize()
-malti2arabi_1char = pn.string_file('mappings/malti2arabi_1char.map').optimize()
-shadda = pn.string_file('mappings/shadda.map').optimize()
-final_vowels = pn.string_file('mappings/final_vowels.map').optimize()
-special = pn.string_file('mappings/special.map').optimize()
 everything_else = pn.string_file('mappings/everything_else.map').optimize()
-alif_initial = pn.string_file('mappings/alif_initial.map').optimize()
-baby_closed_class = pn.string_file('mappings/baby_closed_class.map').optimize()
-baby_closed_class_deterministic = pn.string_file('mappings_deterministic/baby_closed_class_deterministic.map').optimize()
 
-sigma_malti = pn.project(malti2arabi_1char,'input')
+
+sigma_malti = pn.project(malti2arabi_nondet,'input')
 sigma_arabi = pn.project(arabic2arabic,'output') 
 
 # SIGMA
-sigma_in = pn.project(pn.union(malti2arabi_1char,special,arabic2arabic,final_vowels,everything_else),'input')
-sigma = pn.project(pn.union(sigma_in,special,final_vowels),'output').optimize()
+sigma_in = pn.project(pn.union(malti2arabi_nondet,special_nondet,arabic2arabic,final_vowels_nondet,everything_else),'input')
+sigma = pn.project(pn.union(sigma_in,special_nondet,final_vowels_nondet),'output').optimize()
 
-rwr_first_fsts = pn.union(
-    malti2arabi_2char,
-    shadda,
-    final_vowels,
-    alif_initial,
+rwr_first_fsts_nondet = pn.union(
+    malti2arabi_2char_nondet,
+    shadda_nondet,
+    final_vowels_nondet,
+    alif_initial_nondet,
 ).optimize()
 
-rwr_first = pn.cdrewrite(rwr_first_fsts,"","",sigma.closure())
+# rwr_first_nondet = pn.cdrewrite(rwr_first_fsts_nondet,"","",sigma.closure(),mode='opt')
+rwr_first_nondet = pn.cdrewrite(rwr_first_fsts_nondet,"","",sigma.closure())
 
-second_fsts = pn.union(
-    malti2arabi_1char,
+second_fsts_nondet = pn.union(
+    malti2arabi_nondet,
+    special_nondet,
     arabic2arabic, 
-    special,
     everything_else,
 ).optimize()
 
-translit_fst = (rwr_first @ second_fsts.closure()).optimize()
-
+translit_fst_nondet = (rwr_first_nondet @ second_fsts_nondet.closure()).optimize()
+# 
 # deterministic
+malti2arabi_2char_det = pn.string_file('mappings_deterministic/malti2arabi_2char.map').optimize()
 malti2arabi_det= pn.string_file('mappings_deterministic/malti2arabi_1char_vowels_short.map').optimize()
-special_deterministic = pn.string_file('mappings_deterministic/special_deterministic.map').optimize()
+special_det = pn.string_file('mappings_deterministic/special_deterministic.map').optimize()
+shadda_det = pn.string_file('mappings_deterministic/shadda.map').optimize()
 
+
+rwr_first_fsts_det = pn.union(
+    malti2arabi_2char_det,
+    shadda_det,
+).optimize()
+
+# rwr_first_det = pn.cdrewrite(rwr_first_fsts_det,"","",sigma.closure(),mode='opt')
+rwr_first_det = pn.cdrewrite(rwr_first_fsts_det,"","",sigma.closure())
+
+second_fsts_det = pn.union(
+    malti2arabi_det,
+    special_det,
+    arabic2arabic, 
+    everything_else,
+).optimize()
+
+translit_fst_det = (rwr_first_det @ second_fsts_det.closure()).optimize()
+
+#dediac
 diacs = 'ًٌٍَُِّْ'
 dediac_cross = pn.string_file('mappings/dediac.map')
 dediac = pn.cdrewrite(dediac_cross,'','',sigma.closure())
 
+#closed class
 augmented_closed_class = pn.string_file('mappings/augmented_closed_class.map').optimize()
+baby_closed_class = pn.string_file('mappings/baby_closed_class.map').optimize()
+baby_closed_class_deterministic = pn.string_file('mappings_deterministic/baby_closed_class_deterministic.map').optimize()
 
 # words = pn.string_file('../data/arabi_data/tn-maghreb-words.txt').optimize() @ dediac
--- src/translit.py
+++ src/translit.py
@@ -3,13 +3,16 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import strip_accents_unicode
 import kenlm
-from transformers import AutoTokenizer
 import re
-
+from transformers import AutoTokenizer
+# 
 tokenizer = AutoTokenizer.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-mix")
-wordmodel = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_country/lm/word/tn-maghreb.arpa')
-charmodel = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_country/lm/char/tn-maghreb.arpa')
 
+wordmodeltunis = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_country/lm/word/tn-maghreb.arpa')
+charmodeltunis = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_country/lm/char/tn-maghreb.arpa')
+
+wordmodelmaghreb = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_region/lm/word/maghreb.arpa')
+charmodelmaghreb = kenlm.Model('../data/arabi_data/arabic_lm/aggregated_region/lm/char/maghreb.arpa')
 
 def dediac_fst(text):
     text = text.replace('[','\[').replace(']','\]')
@@ -27,66 +30,96 @@ def get_paths(fst,words_only=False):
         return paths
 
 
-def apply_translit_fst(tok,backoff_fsts=[baby_closed_class,augmented_closed_class]):
+def apply_translit_fst_nondet(tok,backoff_fsts=[baby_closed_class,augmented_closed_class]):
     tok = tok.replace('[','\[').replace(']','\]')
     tok = (f'<BOS>{tok}<EOS>')
-    # if type=='det':
-    #     return tok @ deterministic_transcriber @ dediac
     if backoff_fsts:
         backoff =  tok @ pn.union(*backoff_fsts).optimize() @ dediac
         if get_paths(backoff):
             return backoff
         else:
-            return tok  @ translit_fst @ dediac
+            return tok  @ translit_fst_nondet @ dediac
     else:
-        return tok  @ translit_fst @ dediac
+        return tok  @ translit_fst_nondet @ dediac
+    # 
+def apply_translit_fst_det(tok,backoff_fsts=[baby_closed_class,augmented_closed_class]):
+    tok = tok.replace('[','\[').replace(']','\]')
+    tok = (f'<BOS>{tok}<EOS>')
+    if backoff_fsts:
+        backoff =  tok @ pn.union(*backoff_fsts).optimize() @ dediac
+        if get_paths(backoff):
+            return backoff
+        else:
+            return tok  @ translit_fst_det @ dediac
+    else:
+        return tok  @ translit_fst_det @ dediac
 
 def filter_edge_diacritics(options):
     return [y for y in options if y[0] not in diacs and y[-1] not in diacs]
 
-def translit_deterministic(lowered,backoffs=[]):
-    lowered = lowered.replace('[','\[').replace(']','\]') 
-    if backoffs:
-        backofflowered = f'<BOS>{lowered}<EOS>'
-        backoff = backofflowered @ pn.union(*backoffs).optimize() @ dediac
-        if len(get_paths(backoff))==1:
-            return backoff.string()
-        elif len(get_paths(backoff))>1:
-            print('error: fst is NOT deterministic on:',lowered)
-            return '#na'
-    # default
-    try:
-        maptranslit = (lowered @ pn.union(malti2arabi_det,special_deterministic,everything_else).closure().optimize() @ dediac).string()
-        return maptranslit
-    except:
-        print('error detfst on:',lowered)
-        return '#na'
+# def translit_deterministic(lowered,backoffs=[]):
+#     lowered = lowered.replace('[','\[').replace(']','\]') 
+#     if backoffs:
+#         backofflowered = f'<BOS>{lowered}<EOS>'
+#         backoff = backofflowered @ pn.union(*backoffs).optimize() @ dediac
+#         if len(get_paths(backoff))==1:
+#             return backoff.string()
+#         elif len(get_paths(backoff))>1:
+#             print('error: fst is NOT deterministic on:',lowered)
+#             return '#na'
+#     # default
+#     try:
+#         maptranslit = (lowered @ pn.union(malti2arabi_det,special_det,everything_else).closure().optimize() @ dediac).string()
+#         return maptranslit
+#     except:
+#         print('error detfst on:',lowered)
+#         return '#na'
             
 
-def translit_word(lowered_tok,backoffs): #select on merged but return unmerged
+def translit_word(lowered_tok,backoffs,mode): #select on merged but return unmerged
+    if mode=='det':
+        tok_fst = apply_translit_fst_det(lowered_tok,backoffs)
+    elif mode == 'non-det':
+        tok_fst = apply_translit_fst_nondet(lowered_tok,backoffs)
+    else:
+        raise Exception('bad mode, has to be either "det" or "non-det"')
 
-    tok_fst = apply_translit_fst(lowered_tok,backoffs)
     translit_toks = get_paths(tok_fst,words_only=True) 
     if not translit_toks:
-        print('error non-detfst on:',lowered_tok)
+        print(f'error {mode}fst on:',lowered_tok)
         return ['#na']
     try:
         translit_toks = filter_edge_diacritics(translit_toks) 
     except:
-        print('err filtering diacs',translit_toks,lowered_tok)
+        if mode=='det':
+            pass
+        else:
+            print('err filtering diacs',translit_toks,lowered_tok)
     
     translit_toks = [ dediac_fst(x) for x in translit_toks]  # dediacritize
     return translit_toks
 
 
+# cat maghreb.arpa | head -203108 | tail -203097  | cut -f2 > maghreb-words.txt
 
-words_df= pd.read_fwf('../data/arabi_data/tn-maghreb-words.txt',header=None).rename(columns={0:'words'})
-words_df['dediac'] = pd.Series([dediac_fst(x) for x in words_df['words']])
+words_df_maghreb= pd.read_fwf('../data/arabi_data/region_maghreb-words.txt',header=None).rename(columns={0:'words'})
+words_df_maghreb = words_df_maghreb[~words_df_maghreb['words'].isna()]
+words_df_maghreb['dediac'] = pd.Series([dediac_fst(x) for x in words_df_maghreb['words']])
 
-langmodelset =  set(words_df['dediac'])
+words_df_tunis= pd.read_fwf('../data/arabi_data/country_tn-maghreb-words.txt',header=None).rename(columns={0:'words'})
+words_df_tunis = words_df_tunis[~words_df_tunis['words'].isna()]
+words_df_tunis['dediac'] = pd.Series([dediac_fst(x) for x in words_df_tunis['words']])
+
+
+langmodelset_maghreb =  set(words_df_maghreb['dediac'])
+langmodelset_tunis =  set(words_df_tunis['dediac'])
 
 def count_subtokens(text, tokenizer):
     return tokenizer(text, add_special_tokens=False, return_length=True)["length"]
+
+def get_subtoken_ids(text, tokenizer):
+    return list(map(sum, tokenizer(text, add_special_tokens=False, return_length=True)["input_ids"]))
+
 
 def strip_plus(x):
     if x == "+":
@@ -112,9 +145,9 @@ def dediacritise_non_malti_accents(text: str, diacritics_to_keep: str = "ċġħ�
                               + character.group() \
                               + normalised_text[character.end():]
     return normalised_text
+# 
 
-
-def translit_and_rank_options(word,backoffs,name='translit',fsttype='non-det'):
+def translit_and_rank_options(word,backoffs,fsttype,name='translit'):
     normalized = dediacritise_non_malti_accents(word)
     lowered = normalized.lower()
     translit_dict = {
@@ -122,21 +155,20 @@ def translit_and_rank_options(word,backoffs,name='translit',fsttype='non-det'):
         'word_normalized':lowered,
         }
     
-    if fsttype == 'det':
-        translit = [translit_deterministic(lowered,backoffs)]
-    elif fsttype == 'non-det':
-        translit = translit_word(lowered,backoffs)
-    else:
-        raise Exception('wrong fsttype')
+    translit = translit_word(lowered,backoffs,mode=fsttype)
 
     translit_dict[name] = translit
     translit_dict['translit'] = translit # keep this, in order to merge later
     translit_dict['translit_stripped'] = [strip_plus(x) for x in translit]
-    translit_dict['wordmodel_score'] = [wordmodel.score(x) for x in translit_dict['translit_stripped']]
-    translit_dict['charmodel_score'] = [charmodel.score(' '.join(x)) for x in translit_dict['translit_stripped'] ]
     translit_dict['capitalized'] = word[0].isupper() # TODO: what about letter after sink as in 'L-Innu', does it matter?
-    translit_dict['in_langmodel'] = [x in langmodelset for x in translit_dict['translit_stripped']]
+    translit_dict['wordmodel_tunis_score'] = [wordmodeltunis.score(x) for x in translit_dict['translit_stripped']]
+    translit_dict['charmodel_tunis_score'] = [charmodeltunis.score(' '.join(x)) for x in translit_dict['translit_stripped'] ]
+    translit_dict['wordmodel_maghreb_score'] = [wordmodelmaghreb.score(x) for x in translit_dict['translit_stripped']]
+    translit_dict['charmodel_maghreb_score'] = [charmodelmaghreb.score(' '.join(x)) for x in translit_dict['translit_stripped'] ]
+    translit_dict['in_langmodel_tunis'] = [x in langmodelset_tunis for x in translit_dict['translit_stripped']]
+    translit_dict['in_langmodel_maghreb'] = [x in langmodelset_maghreb for x in translit_dict['translit_stripped']]
     translit_dict['subtokens'] = count_subtokens(translit_dict['translit_stripped'], tokenizer)
+    translit_dict['subtoken_ids_sum'] = get_subtoken_ids(translit_dict['translit_stripped'], tokenizer)
     # translit_dict['subtokens_lowest_ties'] = sum(np.array(translit_dict['subtokens']) == min(translit_dict['subtokens']))
 
     return translit_dict
@@ -171,7 +203,7 @@ def merge_multiple(dfs):
     for df in dfs[1:]:
         first = first.merge(df,how='outer')
     
-    return first.sort_values('wordmodel_score',ascending=False)[[
+    return first.sort_values('wordmodel_maghreb_score',ascending=False)[[
         'word_raw',
         'word_normalized',
         'freq',
@@ -183,11 +215,15 @@ def merge_multiple(dfs):
         'nondet_smallcc',
         'nondet_fullcc',        
         'translit_stripped',
-        'wordmodel_score',
-        'charmodel_score',
         'capitalized',
-        'in_langmodel',
+        'wordmodel_tunis_score',
+        'charmodel_tunis_score',
+        'in_langmodel_tunis',
+        'wordmodel_maghreb_score',
+        'charmodel_maghreb_score',
+        'in_langmodel_maghreb',
         'subtokens',
+        'subtoken_ids_sum',
         # 'subtokens_lowest_ties',
         ]]
 
@@ -218,12 +254,12 @@ def translit_dataset(word_hist):
         nondet_fullcclist.append(nondet_fullcc)
         
     
-    detlistdf = pd.DataFrame(detlist).explode(['translit','det','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
-    det_smallcclistdf = pd.DataFrame(det_smallcclist).explode(['translit','det_smallcc','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
-    det_fullcclistdf = pd.DataFrame(det_fullcclist).explode(['translit','det_fullcc','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
-    nondetlistdf = pd.DataFrame(nondetlist).explode(['translit','nondet','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
-    nondet_smallcclistdf = pd.DataFrame(nondet_smallcclist).explode(['translit','nondet_smallcc','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
-    nondet_fullcclistdf = pd.DataFrame(nondet_fullcclist).explode(['translit','nondet_fullcc','translit_stripped','wordmodel_score','charmodel_score','in_langmodel','subtokens'])
+    detlistdf = pd.DataFrame(detlist).explode(['translit','det','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
+    det_smallcclistdf = pd.DataFrame(det_smallcclist).explode(['translit','det_smallcc','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
+    det_fullcclistdf = pd.DataFrame(det_fullcclist).explode(['translit','det_fullcc','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
+    nondetlistdf = pd.DataFrame(nondetlist).explode(['translit','nondet','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
+    nondet_smallcclistdf = pd.DataFrame(nondet_smallcclist).explode(['translit','nondet_smallcc','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
+    nondet_fullcclistdf = pd.DataFrame(nondet_fullcclist).explode(['translit','nondet_fullcc','translit_stripped','wordmodel_tunis_score','wordmodel_maghreb_score','charmodel_tunis_score','charmodel_maghreb_score','in_langmodel_tunis','in_langmodel_maghreb','subtokens','subtoken_ids_sum'])
 
     return merge_multiple(dfs=
                           [
