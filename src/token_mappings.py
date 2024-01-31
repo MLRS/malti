65129ecc61d8110b8bf1ef072543ac54d187c446
Kurt Micallef <kurt.micallef@um.edu.mt>
2024-01-31 08:49:30 +0100

    Handle empty string token mapping.

--- src/token_mappings.py
+++ src/token_mappings.py
@@ -1,3 +1,5 @@
+import csv
+
 TOKEN_MAPPINGS = {}
 
 
@@ -5,8 +7,7 @@ def get_token_mappings(path: str) -> dict[str, str]:
     if path not in TOKEN_MAPPINGS:
         with open(path, "r", encoding="utf-8") as file:
             mappings = {}
-            for line in file:
-                token, mapping = line.strip().split("\t")
+            for token, mapping in csv.reader(file, delimiter="\t", quoting=csv.QUOTE_NONE):
                 mappings[token] = mapping
             TOKEN_MAPPINGS[path] = mappings
     return TOKEN_MAPPINGS[path]
